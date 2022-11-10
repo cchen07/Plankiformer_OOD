eec0a186ff6d2229d70b550a39ded5774b9f026f
Cheng <kimichencheng@icloud.com>
2022-11-10 17:28:05 +0100

    cleaning

---
+++ train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -l
+
+# export CUDA_VISIBLE_DEVICES=1
+
+python3 main.py -datapaths /home/EAWAG/chenchen/data/train_data/new/training_zooplankton_new_220823/ -outpath /home/EAWAG/chenchen/out/train_out/trial/ -classifier multi -aug -datakind image -ttkind image -save_data yes -resize_images 1 -L 128 -valid_set yes -test_set yes -dataset_name zoolake -training_data no -epochs 500 -finetune 2 -finetune_epochs 100 -balance_weight yes -batch_size 2 -init_name Init_0 -run_cnn_or_on_colab yes -use_gpu yes -architecture cnn -last_layer_finetune yes -gpu_id 1 -compute_extrafeat no
